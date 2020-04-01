7d7e0407f66faac82a46702bae5bac7c6716c72f
erwanitwish <61506101+erwanitwish@users.noreply.github.com>
2020-04-01 15:16:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 const dictionnary = ["Dev", "l'UX", "WordPress", "Submit", "Responsive", "React", "Redux", "Bootstrap", "JQuery", "Les props", "Le CSS"];
-const interjections = ["euh", "umf", "parce que", "en fait", "bah", "donc euh", "et donc"]
+const interjections = ["euh", "umf", "parce que", "en fait", "bah", "donc euh", "et donc", "non mais"]
